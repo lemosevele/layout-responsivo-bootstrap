dd446a38ea745631f380d0bdf3903828259fda97
Evele <lemosevele@gmail.com>
2021-02-25 12:37:25 -0300

    projeto do curso de design responsivo - nodestudio

---
+++ nodeprop/index.js
@@ -0,0 +1,11 @@
+const Menu = {
+
+    open(){
+        document.querySelector(".menu").classList.add("active");
+    },
+
+    close(){
+        document.querySelector(".menu").classList.remove("active");
+    }
+    
+}
